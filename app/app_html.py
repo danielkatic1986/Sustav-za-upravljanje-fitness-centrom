765fc1519bc81cc6186f47d0459fdbf26222eb6d
danielkatic1986 <dkatic@unipu.hr>
2026-01-11 15:34:32 +0100

    implementirao prikaz i dodavanje člana u flask app

--- app/app_html.py
+++ app/app_html.py
@@ -1,3 +1,8 @@
+# ---------------------------------------
+#    Flask aplikacija – Fitness centar
+#       HTML sučelje (BP2 projekt)
+# ---------------------------------------
+
 from flask import Flask, render_template, request, redirect, url_for
 from flask_mysqldb import MySQL
 
@@ -16,6 +21,66 @@ mysql = MySQL(app)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/clanovi")
+def clanovi():
+    cur = mysql.connection.cursor()
+    cur.execute("""
+        SELECT
+            c.id,
+            c.ime,
+            c.prezime,
+            m.naziv,
+            c.datum_uclanjenja,
+            c.aktivan
+        FROM clan c
+        JOIN mjesto m ON c.id_mjesto = m.id
+        ORDER BY c.id DESC
+    """)
+    clanovi = cur.fetchall()
+    cur.close()
+
+    return render_template("clanovi.html", clanovi=clanovi)
+
+@app.route("/clanovi/novi", methods=["GET", "POST"])
+def clan_novi():
+
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT id, naziv, postanski_broj, drzava FROM mjesto")
+    mjesta = cur.fetchall()
+    cur.close()
+
+    if request.method == "POST":
+        ime = request.form["ime"]
+        prezime = request.form["prezime"]
+        oib = request.form["oib"]
+        spol = request.form["spol"]
+        datum_rodenja = request.form["datum_rodenja"]
+        id_mjesto = request.form["id_mjesto"]
+        adresa = request.form["adresa"]
+        email = request.form["email"]
+        telefon = request.form["telefon"]
+
+        cur = mysql.connection.cursor()
+        cur.execute("""
+            INSERT INTO clan
+            (ime, prezime, oib, spol, datum_rodenja, id_mjesto, adresa, email, telefon,
+             datum_uclanjenja, datum_posljednje_aktivnosti, aktivan)
+            VALUES
+            (%s, %s, %s, %s, %s, %s, %s, %s, %s,
+             CURDATE(), CURDATE(), 1)
+        """, (
+            ime, prezime, oib, spol, datum_rodenja,
+            id_mjesto, adresa, email, telefon
+        ))
+
+        mysql.connection.commit()
+        cur.close()
+
+        return redirect(url_for("clanovi"))
+
+    return render_template("clan_novi.html", mjesta=mjesta)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
